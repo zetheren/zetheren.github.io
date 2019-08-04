748a272930179fcc9067146383f145f475556911
zetheren <53668127+zetheren@users.noreply.github.com>
2019-08-03 21:32:31 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1564881765,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/up-sheet0.png",
+		"images/right_touch-sheet0.png",
+		"images/left_touch-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/flagblue-sheet0.png",
+		"images/flagblue-sheet1.png",
+		"images/flagblue-sheet2.png",
+		"images/flaggreen-sheet0.png",
+		"images/flaggreen-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
